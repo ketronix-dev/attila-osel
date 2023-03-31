5e4ca845013305ef7950ae9b1fd2d7a1e5375ce7
Жора Змейкин <rs5334819@gmail.com>
2023-04-01 01:56:17 +0400

    Removing cover-active. What's it for, anyway?

--- src/js/script.js
+++ src/js/script.js
@@ -53,7 +53,6 @@ jQuery(function($) {
         '-webkit-transform': 'translate3d(0, ' + coverPosition + 'px, 0)',
         'transform': 'translate3d(0, ' + coverPosition + 'px, 0)'
       });
-      (viewport.scrollTop() < cover.height()) ? html.addClass('cover-active'): html.removeClass('cover-active');
     }
   }
   prlx();
