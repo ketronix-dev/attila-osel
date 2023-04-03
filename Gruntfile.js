b56fdb1d803fe928230e9be9437c0815ae44d4f9
Жора Змейкин <rs5334819@gmail.com>
2023-04-04 00:28:51 +0400

    Libraries are now stored locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt) {
       'jsDependencies': [
         '<%= config.jsSrcDir %>/libs/jquery.min.js',
         '<%= config.jsSrcDir %>/libs/jquery.fitvids.js',
-        '<%= config.jsSrcDir %>/libs/highlight.pack.js'
+        '<%= config.jsSrcDir %>/libs/highlight.min.js',
+        '<%= config.jsSrcDir %>/libs/clipboard.min.js'
       ]
     },
     copy: {
